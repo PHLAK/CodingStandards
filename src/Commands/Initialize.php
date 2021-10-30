517fe661a506859043c5d60b0299737a3d47e23b
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-10-29 23:21:17 -0700

    Fixed some static analysis issues

--- src/Commands/Initialize.php
+++ src/Commands/Initialize.php
@@ -3,6 +3,7 @@
 namespace PHLAK\CodingStandards\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -30,22 +31,24 @@ class Initialize extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         if (file_exists(self::CONFIG_FILE_NAME)) {
+            /** @var QuestionHelper $helper */
+            $helper = $this->getHelper('question');
             $question = new ConfirmationQuestion('<question>Configuration file already exists. Overwrite?</question> (y|N) ', false);
 
-            if (! $this->getHelper('question')->ask($input, $output, $question)) {
+            if (! $helper->ask($input, $output, $question)) {
                 $output->writeln('<fg=yellow>Aborted!</>');
 
                 return Command::SUCCESS;
             }
         }
 
-        if (! copy(self::CONFIG_FILE_STUB, $input->getArgument('path') . '/' . self::CONFIG_FILE_NAME)) {
+        if (! copy(self::CONFIG_FILE_STUB, (string) $input->getArgument('path') . '/' . self::CONFIG_FILE_NAME)) {
             $output->writeln('<error>Failed to initialze coding standards configuration</error>');
 
             return self::FAILURE;
         }
 
-        $output->writeln(sprintf('<info>Initialized coding standards configuration in %s</info>', $input->getArgument('path')));
+        $output->writeln(sprintf('<info>Initialized coding standards configuration in %s</info>', (string) $input->getArgument('path')));
 
         return self::SUCCESS;
     }
